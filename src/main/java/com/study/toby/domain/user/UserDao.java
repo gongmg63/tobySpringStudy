545bc837d60e19e03969ea114b0f9d140623b411
mkong <rhdalsrn2@naver.com>
2025-01-25 14:51:43 +0900

    [MOD] 초난감 DAO 수정 UserDao 에서 하나에 집중된 관심사항을 분리하기 -커넥션을 가져오는 중복된 코드를 분리 -템플릿 메소드 패턴을 적용 -getConnection()함수 생성

--- src/main/java/com/study/toby/domain/user/UserDao.java
+++ src/main/java/com/study/toby/domain/user/UserDao.java
@@ -4,10 +4,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/toby", "mkong", "1234"
-        );
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)"
@@ -23,10 +20,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/toby", "mkong", "1234"
-        );
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
@@ -46,4 +40,37 @@ public class UserDao {
 
         return user;
     }
+
+    // proteced 로 선언해 서브클래스에서 선택적으로 오버라이드할 수 있게해 확장성을 높일 수 있음 = 훅 메소드
+    protected Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby", "mkong", "1234"
+        );
+    }
+    // 추상메소드로 선언해 서브클래스에서 반드시 구현하게 해 확장성을 높이면서 기존 코드를 은닉할 수 있음
+//    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
+
+//public class NUserDao extends UserDao {
+//    protected Connection getConnection() throws ClassNotFoundException, SQLException{
+//        // N사 DB Connection 생성코드
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//        return DriverManager.getConnection(
+//                "jdbc:mysql://localhost/toby", "mkong", "1234"
+//        );
+//    }
+//}
+//
+//public class DUserDao extends UserDao {
+//    protected Connection getConnection() throws ClassNotFoundException, SQLException{
+//        // D사 DB Connection 생성코드
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//        return DriverManager.getConnection(
+//                "jdbc:mysql://localhost/toby", "mkong", "1234"
+//        );
+//    }
+//}
